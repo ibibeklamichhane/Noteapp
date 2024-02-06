8e7d7f59fc0a4877cc2f4c5f1f2174908a9d4e15
ibibeklamichhane <bibeklc714@gmail.com>
2024-02-06 14:28:47 +0545

    modified package.json

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -5,13 +5,13 @@ var cors = require('cors')
 
 connectToMongo();
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5002
 
 app.use(cors())
 app.use(express.json())
 
 //available routes
-app.get('/',(req,res) => res.status(200).json({message:'Hello World'}))
+
 app.use ('/api/auth',require('./routes/auth'))
 app.use ('/api/notes',require('./routes/notes'))
 
